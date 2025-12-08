ec3fa071e2e182316e88bdd473859c2ee75f9898
fuma-ops <khfuma@gmail.com>
2025-12-08 15:34:11 +0000

    removed additional page

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -20,12 +20,7 @@ export default function App() {
 
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <NavMenu>
-        <Link to="/app" rel="home">
-          Home
-        </Link>
-        <Link to="/app/additional">Additional page</Link>
-      </NavMenu>
+      
       <Outlet />
     </AppProvider>
   );
